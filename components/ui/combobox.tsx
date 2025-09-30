00923a338e45ea0b36ae64f30be0899d3f6ec61f
Abayommy <108365023+Abayommy@users.noreply.github.com>
2025-09-30 00:23:29 -0400

    Remove align prop from PopoverContent in combobox

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -77,7 +77,7 @@ export function Combobox(props: ComboboxProps) {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-0" align="start">
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput 
             placeholder={searchPlaceholder}
@@ -91,7 +91,7 @@ export function Combobox(props: ComboboxProps) {
                   className="px-2 py-2 cursor-pointer hover:bg-gray-100 rounded-sm"
                   onClick={() => handleSelect(searchValue)}
                 >
-                  Use "{searchValue}"
+                  Use &quot;{searchValue}&quot;
                 </div>
               ) : (
                 emptyText
